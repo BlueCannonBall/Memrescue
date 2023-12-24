a602daa12c0b86ce6fee6decb268cbf29f7d296b
BlueCannonBall <mmttiizz100@gmail.com>
2023-12-24 18:54:26 -0500

    Prevent all processes from being killed

--- config.hpp
+++ config.hpp
@@ -4,9 +4,10 @@
 #define LOG_FILE "/dev/kmsg" // File path to write logs to
 
 // Functionality
-#define SLEEP_TIME       500  // Amount of time in milliseconds to sleep before checking iowait again
-#define MAX_MEMORY_USAGE 0.95 // Maximum percent of memory being used allowed before acting
-#define IOWAIT_THRES     0.25 // Maximum percent of CPU time dedicated to IO allowed before acting
+#define IOWAIT_SLEEP_TIME 500  // Amount of time in milliseconds to sleep before checking iowait again
+#define MEMORY_SLEEP_TIME 100  // Amount of time in milliseconds to sleep before checking memory usage again
+#define MAX_MEMORY_USAGE  0.95 // Maximum percent of memory being used allowed before acting
+#define IOWAIT_THRES      0.25 // Maximum percent of CPU time dedicated to IO allowed before acting
 
 // Timers (in milliseconds)
 #define CACHE_TIMEOUT 10000
--- main.cpp
+++ main.cpp
@@ -19,31 +19,42 @@ int main() {
 
     for (;;) {
         CPUStats start_cpu_stats = manager.cpu_stats();
-        std::this_thread::sleep_for(std::chrono::milliseconds(SLEEP_TIME));
+        std::this_thread::sleep_for(std::chrono::milliseconds(IOWAIT_SLEEP_TIME));
         CPUStats end_cpu_stats = manager.cpu_stats();
-        MemoryInfo memory_info = manager.info();
         auto now = std::chrono::steady_clock::now();
 
         double iowait_percentage = (double) (end_cpu_stats.iowait_time - start_cpu_stats.iowait_time) / (end_cpu_stats.total_time - start_cpu_stats.total_time);
-        double memory_percentage = 1. - (double) memory_info.available / memory_info.total;
-
 #ifdef DEBUG
-        manager.log("{ iowait_percentage: " + std::to_string(iowait_percentage) + ", memory_percentage: " + std::to_string(memory_percentage) + " }");
+        manager.log("iowait_percentage: " + std::to_string(iowait_percentage));
 #endif
-
         if (iowait_percentage > IOWAIT_THRES) {
+            MemoryInfo memory_info = manager.info();
+            double memory_percentage = 1. - (double) memory_info.available / memory_info.total;
+#ifdef DEBUG
+            manager.log("memory_percentage: " + std::to_string(memory_percentage));
+#endif
             if (memory_percentage > MAX_MEMORY_USAGE) {
+                if (now - manager.clears.cache > std::chrono::milliseconds(CACHE_TIMEOUT) && memory_info.cached) {
+                    manager.drop_caches();
+                }
+
+                // Update memory info
+                memory_info = manager.info();
+                memory_percentage = 1. - (double) memory_info.available / memory_info.total;
+
                 auto oom_scores = manager.get_oom_scores();
-                do {
+                while (memory_percentage > MAX_MEMORY_USAGE && !oom_scores.empty()) {
                     auto highest_process = std::max_element(oom_scores.begin(), oom_scores.end(), [](const auto& a, const auto& b) {
                         return a.second < b.second;
                     });
                     manager.kill_process(highest_process->first);
                     oom_scores.erase(highest_process);
-                } while (memory_percentage > MAX_MEMORY_USAGE);
 
-                if (now - manager.clears.cache > std::chrono::milliseconds(CACHE_TIMEOUT) && memory_info.cached) {
-                    manager.drop_caches();
+                    std::this_thread::sleep_for(std::chrono::milliseconds(MEMORY_SLEEP_TIME));
+
+                    // Update memory info
+                    memory_info = manager.info();
+                    memory_percentage = 1. - (double) memory_info.available / memory_info.total;
                 }
             } else if (now - manager.clears.swap > std::chrono::milliseconds(SWAP_TIMEOUT) &&
                        memory_info.total_swap &&
