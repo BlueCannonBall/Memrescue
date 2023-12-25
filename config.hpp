218022f2c0a5c152c3d2dc8d539cecb3eb3b8d9a
BlueCannonBall <mmttiizz100@gmail.com>
2023-12-24 22:19:46 -0500

    Change default OOM_KILLER_SAMPLE_SIZE

--- config.hpp
+++ config.hpp
@@ -8,7 +8,7 @@
 #define MEMORY_SLEEP_TIME      100  // Amount of time in milliseconds to sleep before checking memory usage again
 #define MAX_MEMORY_USAGE       0.95 // Maximum percent of memory being used allowed before acting
 #define IOWAIT_THRES           0.25 // Maximum percent of CPU time dedicated to IO allowed before acting
-#define OOM_KILLER_SAMPLE_SIZE 3    // The number of processes that are sampled at a time when killing processes
+#define OOM_KILLER_SAMPLE_SIZE 5    // The number of processes that are sampled at a time when killing processes
 
 // Timers (in milliseconds)
 #define CACHE_TIMEOUT 10000
